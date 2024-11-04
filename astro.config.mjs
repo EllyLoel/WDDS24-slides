36bb5d7f64706ac1d2c19cab07ddbd65841c063f
Elly Loel <hello@ellyloel.com>
2024-11-04 13:20:51 +1100

    Fixing code blocks some more

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,15 @@
 import { defineConfig } from 'astro/config';
+import { transformerNotationDiff } from '@shikijs/transformers';
 
-export default defineConfig();
+export default defineConfig({
+	markdown: {
+		shikiConfig: {
+			themes: {
+				light: 'github-light-high-contrast',
+				dark: 'github-dark-high-contrast',
+			},
+			wrap: true,
+			transformers: [transformerNotationDiff()],
+		},
+	},
+});
